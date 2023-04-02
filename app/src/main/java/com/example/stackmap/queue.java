d9059e4c621e2954e112e6166172e23a59d53b86
dhanush678 <dhanugowda200326@gmail.com>
2023-04-02 16:27:48 +0530

    adding notification

--- app/src/main/java/com/example/stackmap/queue.java
+++ app/src/main/java/com/example/stackmap/queue.java
@@ -1,8 +1,14 @@
 package com.example.stackmap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.example.queue.circular_queue;
+import com.example.queue.double_queue;
+import com.example.queue.priority_queue;
+import com.google.android.material.tabs.TabLayout;
 
 public class queue extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class queue extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_queue);
+
+        ViewPagerAdapter viewPagerAdapter;
+        ViewPager viewPager;
+        TabLayout tabLayout;
+        viewPager = findViewById(R.id.viewpager);
+
+        // setting up the adapter
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        // add the fragments
+        viewPagerAdapter.add(new com.example.queue.queue(), "Linear");
+        viewPagerAdapter.add(new circular_queue(), "Circular");
+        viewPagerAdapter.add(new double_queue(), "Double");
+
+        viewPagerAdapter.add(new priority_queue(), "Priority");
+
+
+
+
+        // Set the adapter
+        viewPager.setAdapter(viewPagerAdapter);
+
+        // The Page (fragment) titles will be displayed in the
+        // tabLayout hence we need to  set the page viewer
+        // we use the setupWithViewPager().
+        tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- app/src/main/java/com/example/stackmap/tree.java
+++ app/src/main/java/com/example/stackmap/tree.java
@@ -1,14 +1,39 @@
 package com.example.stackmap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.example.queue.circular_queue;
+import com.example.queue.double_queue;
+import com.example.queue.priority_queue;
+import com.google.android.material.tabs.TabLayout;
 
 public class tree extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tree);
+        setContentView(R.layout.activity_queue);
+
+        ViewPagerAdapter viewPagerAdapter;
+        ViewPager viewPager;
+        TabLayout tabLayout;
+        viewPager = findViewById(R.id.viewpager);
+
+        // setting up the adapter
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+
+
+        // Set the adapter
+        viewPager.setAdapter(viewPagerAdapter);
+
+        // The Page (fragment) titles will be displayed in the
+        // tabLayout hence we need to  set the page viewer
+        // we use the setupWithViewPager().
+        tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
